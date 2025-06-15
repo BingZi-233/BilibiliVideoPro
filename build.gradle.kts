c8741928c6948280448fa6094af59b1582ff6209
BingZi-233 <lhby233@outlook.com>
2025-06-15 14:25:58 +0800

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,65 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import io.izzel.taboolib.gradle.*
+import io.izzel.taboolib.gradle.Basic
+import io.izzel.taboolib.gradle.Bukkit
+import io.izzel.taboolib.gradle.BukkitHook
+import io.izzel.taboolib.gradle.CommandHelper
+import io.izzel.taboolib.gradle.I18n
+import io.izzel.taboolib.gradle.Metrics
+import io.izzel.taboolib.gradle.MinecraftChat
+
+
+plugins {
+    java
+    id("io.izzel.taboolib") version "2.0.22"
+    id("org.jetbrains.kotlin.jvm") version "1.8.22"
+}
+
+taboolib {
+    env {
+        install(Basic)
+        install(Bukkit)
+        install(BukkitHook)
+        install(CommandHelper)
+        install(I18n)
+        install(Metrics)
+        install(MinecraftChat)
+    }
+    description {
+        name = "BilibiliVideoPro"
+        contributors {
+            name("BingZi-233")
+        }
+        links {
+            name("https://www.bingzi.online")
+        }
+    }
+    version { taboolib = "6.2.3-20d868d" }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("ink.ptms.core:v12004:12004:mapped")
+    compileOnly("ink.ptms.core:v12004:12004:universal")
+    compileOnly(kotlin("stdlib"))
+    compileOnly(fileTree("libs"))
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = listOf("-Xjvm-default=all")
+    }
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
---
+++ src/main/kotlin/online/bingzi/bilibili/video/pro/BilibiliVideoPro.kt
@@ -0,0 +1,11 @@
+package online.bingzi.bilibili.video.pro
+
+import taboolib.common.platform.Plugin
+import taboolib.common.platform.function.info
+
+object BilibiliVideoPro : Plugin() {
+
+    override fun onEnable() {
+        info("Successfully running BilibiliVideoPro!")
+    }
+}
